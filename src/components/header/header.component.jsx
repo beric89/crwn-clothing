7688f56232b74d41ec7e9f96d4aba99ace58f23e
Milan Beric <bericmilan89@yahoo.com>
2020-11-03 00:45:45 +0100

    Implement firebase utils, include ability to store authenticated users

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -19,7 +19,7 @@ const Header = ({currentUser}) => (
                 currentUser ?
                 <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
                 :
-                <Link class="option" to="/signin">SIGN IN</Link>
+                <Link className="option" to="/signin">SIGN IN</Link>
             }
         </div>
     </div>
